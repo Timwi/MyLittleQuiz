0800c1ed1c643186d64ba5bdf2f525580844d4a6
Timwi <github@timwi.de>
2016-03-13 03:27:00 +0100

    Commit all the source files (Cakewalk, POV, etc.) and move generated stuff to a subfolder and .gitignore it

---
+++ Src/Resources/MyLittleQuiz/Generate POVs.bat
@@ -0,0 +1,13 @@
+D:\c\PovRounding\Builds\Release\PovRounding.exe -r 5 -f 0.7 -d 40 -sf -o "Generated\MyHeart.pov"      -n MyHeart      svgpath -f "My Little Quiz logo.svg" -i my-heart
+D:\c\PovRounding\Builds\Release\PovRounding.exe -r 5 -f 0.7 -d 42 -sf -o "Generated\My.pov"           -n My           svgpath -f "My Little Quiz logo.svg" -i my
+D:\c\PovRounding\Builds\Release\PovRounding.exe -r 5 -f 0.7 -d 50 -sf -o "Generated\Little.pov"       -n Little       svgpath -f "My Little Quiz logo.svg" -i little
+D:\c\PovRounding\Builds\Release\PovRounding.exe -r 5 -f 0.7 -d 60 -sf -o "Generated\Quiz.pov"         -n Quiz         svgpath -f "My Little Quiz logo.svg" -i quiz
+D:\c\PovRounding\Builds\Release\PovRounding.exe -r 5 -f 0.7 -d 25 -sf -o "Generated\MyCloud.pov"      -n MyCloud      svgpath -f "My Little Quiz logo.svg" -i my-cloud
+D:\c\PovRounding\Builds\Release\PovRounding.exe -r 5 -f 0.7 -d 25 -sf -o "Generated\LittleCloud.pov"  -n LittleCloud  svgpath -f "My Little Quiz logo.svg" -i little-cloud
+D:\c\PovRounding\Builds\Release\PovRounding.exe -r 5 -f 0.7 -d 25 -sf -o "Generated\QuizCloud.pov"    -n QuizCloud    svgpath -f "My Little Quiz logo.svg" -i quiz-cloud
+D:\c\PovRounding\Builds\Release\PovRounding.exe -r 5 -f 0.7 -d 15 -sf -o "Generated\RainbowCloud.pov" -n RainbowCloud svgpath -f "My Little Quiz logo.svg" -i rainbow-cloud
+D:\c\PovRounding\Builds\Release\PovRounding.exe -r 5 -f 0.7 -d 25 -sf -o "Generated\Rainbow1.pov"     -n Rainbow1     svgpath -f "My Little Quiz logo.svg" -i rainbow1
+D:\c\PovRounding\Builds\Release\PovRounding.exe -r 5 -f 0.7 -d 23 -sf -o "Generated\Rainbow2.pov"     -n Rainbow2     svgpath -f "My Little Quiz logo.svg" -i rainbow2
+D:\c\PovRounding\Builds\Release\PovRounding.exe -r 5 -f 0.7 -d 21 -sf -o "Generated\Rainbow3.pov"     -n Rainbow3     svgpath -f "My Little Quiz logo.svg" -i rainbow3
+D:\c\PovRounding\Builds\Release\PovRounding.exe -r 5 -f 0.7 -d 19 -sf -o "Generated\Rainbow4.pov"     -n Rainbow4     svgpath -f "My Little Quiz logo.svg" -i rainbow4
+D:\c\PovRounding\Builds\Release\PovRounding.exe -r 5 -f 0.7 -d 17 -sf -o "Generated\Rainbow5.pov"     -n Rainbow5     svgpath -f "My Little Quiz logo.svg" -i rainbow5
